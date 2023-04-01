bcdc7893db7b1a1b256029b27fcf48230e8a2a9a
Not-user-1984 <deman.tmb.68@gmail.com>
2023-04-01 11:54:49 +0300

    events add funs event and fitchText,fitchTepe

--- clients/telegram/types.go
+++ clients/telegram/types.go
@@ -1,12 +1,17 @@
 package telegram
-// UpdatesResponse имеет два поля: Ok, которое является булевой переменной, 
-// указывающей на успешность выполнения запроса, и Result, 
-// который представляет собой массив объектов типа Update.
-
-// Update, в свою очередь,
-// представляет собой единичное обновление в чате Telegram и имеет два поля:
-// ID - уникальный идентификатор обновления,
-// и Message - текст сообщения.
+// Этот код определяет структуры данных для десериализации 
+// JSON-ответа API Telegram на запрос getUpdates.
+// Структура UpdatesResponse содержит поле Ok,
+// которое указывает, успешно ли был выполнен запрос,
+// и поле Result, которое содержит массив объектов типа Update.
+// Каждый объект типа Update содержит поле ID,
+// которое представляет уникальный идентификатор обновления,
+// и поле Message, которое ссылается на объект типа IncomingMessage.
+// Объект типа IncomingMessage содержит поле Text,
+// которое содержит текст сообщения, поле From,
+// которое ссылается на объект типа From с информацией об отправителе сообщения (например, его имя пользователя), и поле Chat,
+// которое ссылается на объект типа Chat с информацией о чате, в котором было получено сообщение.
+// Объекты типа From и Chat содержат дополнительную информацию о пользователе и чате соответственно.
 
 type UpdatesResponse struct {
     Ok      bool   `json:"ok"`
@@ -14,5 +19,19 @@ type UpdatesResponse struct {
 }
 type Update struct {
     ID      int    `json:"update_id"`
-    Message string `json:"message"`
+    Message *IncomingMessage `json:"message"`
+}
+
+type IncomingMessage struct{
+    Text string  `json:"text"`
+    From From    `json:"from"`
+    Chat Chat    `json:"chat"` 
+}
+
+type From struct {
+    Username string `json:"username"`
+}
+
+type Chat struct {
+    ID int `json:"id"`
 }
--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -2,11 +2,57 @@ package telegram
 
 import (
 	"telegram_bot_link/clients/telegram"
+	"telegram_bot_link/events"
+	"telegram_bot_link/lib/e"
+	"telegram_bot_link/storage"
 )
 
 type Processor struct {
 	tg *telegram.Client
 	offset int
+	storage storage.Storage
 }
 
-func New (client *telegram.Client, storage)
+func New(client *telegram.Client, storage storage.Storage) *Processor{
+	return &Processor{
+		tg: client,
+		storage: storage,
+	}
+}
+func(p *Processor) Fetch(limit int)([]events.Event,error) {
+	update, err := p.tg.Updates(p.offset,limit)
+	if err!= nil {
+		return nil, e.Wrap("cant get events", err)
+	}
+	res := make([]events.Event,0,len(update))
+
+	for _, u := range update{
+		res=append(res, event(u))
+	}
+
+}
+
+func event(upd telegram.Update) string {
+	updType := fetchType(upd)
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+	if updType==events. {}
+	
+}
+
+func fetchType(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+	return upd.Message.Text
+
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return events.Unknow
+	}
+	return upd.Message.Text
+}
